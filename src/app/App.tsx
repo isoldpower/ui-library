a06fa5b24b3da62d7f661398cef177116e4b56be
isoldpower <Lapin_NR@icloud.com>
2024-04-26 19:20:58 +0500

    feat: basic app structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,19 @@ import {Outlet} from 'react-router-dom';
 export const App = () => {
 
     return (
-        <Outlet />
+        <>
+            <header className={`${classes.app__header}`}>
+                <div className={`${classes.app__headerContent}`}></div>
+                <div className={`${classes.app__headerModals}`}></div>
+            </header>
+            <main className={`${classes.app__main}`}>
+                <div className={`${classes.app__mainContent}`}>
+                    <Outlet />
+                </div>
+                <div className={`${classes.app__modals}`}></div>
+                <div className={`${classes.app__notifications}`}></div>
+            </main>
+            <footer className={`${classes.app__footer}`}></footer>
+        </>
     );
 };
